8284c18d9a0509ac4d3cda9b699179725f62f980
miguel <fernando.duarte@hsjoao.min-saude.pt>
2016-01-27 14:39:20 +0000

    Initial Commit

---
+++ iBeacons/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iBeacons
+//
+//  Created by Fernando Miguel Oliveira Duarte on 27/01/16.
+//  Copyright © 2016 AS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
